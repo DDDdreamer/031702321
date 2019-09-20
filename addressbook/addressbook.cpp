58932f9765fefd87c6a819869cfe475f0778a605
lxccccc <50549978+DDDdreamer@users.noreply.github.com>
2019-09-21 02:11:26 +0800

    Add files via upload

--- addressbook/addressbook.cpp
+++ addressbook/addressbook.cpp
@@ -377,14 +377,14 @@ public:
 
 int main(int argc, char **argv)
 {
-	ifstream input;
-	ofstream output;
-	input.open(argv[1]);
-	output.open(argv[2]);
 	/*ifstream input;
 	ofstream output;
-	input.open("E:\\SoftwareTest-1.0.0\\dl\\lxc\\input.txt");
-	output.open("E:\\SoftwareTest-1.0.0\\dl\\lxc\\output.txt");*/
+	input.open(argv[1]);
+	output.open(argv[2]);*/
+	ifstream input;
+	ofstream output;
+	input.open("F:\\input.txt");
+	output.open("F:\\output.txt");
 	vector<string> address;
 	int count = 0;
 	string s;
